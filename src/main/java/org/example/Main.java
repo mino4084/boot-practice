2e509f79579acce6cec3712bbfc0144f0559e5c9
mino <mino4084@gmail.com>
2023-04-18 21:01:20 +0900

    메인 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
-package org.example;public class Main {
+package org.example;
+
+import org.example.logic.BubbleSort;
+
+import java.util.Arrays;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BubbleSort<String> sort = new BubbleSort<>();
+        System.out.println(sort.sort(Arrays.asList(args)));
     }
 }

8dbc781a0800d394256f5df8c2062d2418f5ae0a
mino <mino4084@gmail.com>
2023-04-18 21:00:27 +0900

    버블소트 구현

---
+++ src/test/java/org/example/logic/BubbleSortTest.java
@@ -0,0 +1,26 @@
+package org.example.logic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest {
+
+    @DisplayName("버블소트 - 리스트를 넣으면 정렬된 결과를 받는다.")
+    @Test
+    void given_List_WhenExecuting_ThenReturnSortedList(){
+        //given
+        BubbleSort<Integer> bubbleSort = new BubbleSort<>();
+
+        //when
+        List<Integer> actual = bubbleSort.sort(List.of(3, 2, 5, 1, 4, 6));
+
+        //then
+        assertEquals(List.of(1, 2, 3, 4, 5, 6), actual);
+
+
+    }
+}

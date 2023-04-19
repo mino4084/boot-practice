d1217b8abd05a3ed1d8c353bb4a26ec0bf35b7bb
mino <mino4084@gmail.com>
2023-04-19 09:23:48 +0900

    Sort 인터페이스 개발

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,20 @@
 package org.example;
 
 import org.example.logic.BubbleSort;
+import org.example.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+
+        // 구현체에 의존
+        // BubbleSort<String> sort = new BubbleSort<>();
+
+        // 인터페이스에 의존, 구현체만 갈아끼움
+        Sort<String> sort = new BubbleSort<>();
+        //Sort<String> sort = new JavaSort<>();
+
         System.out.println(sort.sort(Arrays.asList(args)));
     }
 }

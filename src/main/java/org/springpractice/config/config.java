0426cc1373a43d7d5246cc3b1af7619d08b4875f
mino <mino4084@gmail.com>
2023-04-27 09:27:20 +0900

    config 설정 추가
    루트 패키지 이하 빈 스캔 설정

---
+++ src/main/java/org/springpractice/config/config.java
@@ -0,0 +1,10 @@
+package org.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("org.springpractice")
+@Configuration
+public class config {
+
+}
